9fd20026e6481c36f70b0426caa1e93e63f16f83
Travis Mewborne <tmewborne@hotmail.com>
2021-07-07 15:36:23 -0400

    able to located paired bluetooth devices, but not connected devices

--- app/src/main/java/com/example/bluetoothwristpostiontracker/MainActivity.java
+++ app/src/main/java/com/example/bluetoothwristpostiontracker/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.bluetoothwristpostiontracker;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.bluetoothwristpostiontracker.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     private TextView txtBluetoothInfo;
     private TableLayout tblBluetoothData;
     private ActivityMainBinding binding;
+    private MyBluetoothManager btMan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,25 @@ public class MainActivity extends Activity {
 
         txtBluetoothInfo = binding.txtBluetoothInfo;
         tblBluetoothData = binding.tblBluetoothData;
+
+        btMan = new MyBluetoothManager(this,this);
+        updateTable();
+    }
+
+
+    public void updateTable(){
+        ArrayList<BluetoothDevice> devices = btMan.getConnectedDevices();
+        for (BluetoothDevice device:devices){
+            TableRow row = new TableRow(this);
+            TextView txtName = new TextView(this);
+            TextView textRSSI = new TextView(this);
+            txtName.setText(device.getName());
+            //textRSSI.setText(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE));
+            row.addView(txtName);
+            row.addView(textRSSI);
+            tblBluetoothData.addView(row);
+        }
+        if (devices.size()!=0) txtBluetoothInfo.setText("Connected Devices");
+        else txtBluetoothInfo.setText("No Connected Devices\nPlease connect 2+ devices");
     }
 }
--- app/src/main/java/com/example/bluetoothwristpostiontracker/MyBluetoothManager.java
+++ app/src/main/java/com/example/bluetoothwristpostiontracker/MyBluetoothManager.java
@@ -1,6 +1,101 @@
 package com.example.bluetoothwristpostiontracker;
 
 
-//Used for devices that are already paired
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCallback;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+//Used for devices that are already paired to the device
 public class MyBluetoothManager {
+    private BluetoothAdapter bluetoothAdapter;
+    private Context context;
+    private boolean readyToSearch;
+    private ArrayList<BluetoothDevice> connectedDevices;
+    private ArrayList<BluetoothDevice> pairedDevices;
+    private String debugTag = "MyBluetoothManager";
+    private MainActivity parent;
+
+    public MyBluetoothManager(Context context,MainActivity parent) {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        this.context=context;
+        this.parent = parent;
+        connectedDevices = new ArrayList<BluetoothDevice>();
+        pairedDevices = new ArrayList<BluetoothDevice>();
+        Log.d(debugTag,"MyBluetoothManager -- Object Created");
+
+        readyToSearch = bluetoothTestAndEnable();
+        if (!readyToSearch) {
+            Log.e(debugTag,"getPairedDevices -- Failed to enable bluetooth");
+        } else {
+            pairedDevices.addAll(bluetoothAdapter.getBondedDevices());//Returns PAIRED devices
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+            context.registerReceiver(mReceiver, filter);
+            Log.d(debugTag,"constructor -- (" + pairedDevices.size() + ") paired device(s) were located");
+            for(BluetoothDevice device : pairedDevices) {
+                //device.connectGatt(context, true, new BluetoothGattCallback() {});
+                boolean bonded = device.createBond();
+                Log.d(debugTag,"constructor -- attempting to bond to \"" + device.getName() + "\"..." + (bonded ? "success" : "failed"));
+            }
+        }
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //Device found
+            }
+            else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                //Device has connected
+                connectedDevices.add(device);
+                Log.d(debugTag,"onReceive -- a device was located");
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //Done searching
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
+                //Device is about to disconnect
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                //Device has disconnected
+                connectedDevices.remove(device);
+            }
+
+            parent.updateTable();
+        }
+    };
+
+    private boolean bluetoothTestAndEnable() {
+        if (bluetoothAdapter == null) {
+            Log.e(debugTag,"bluetoothTestAndEnable -- This device does not have a bluetooth adapter. Cannot continue.");
+            return false;
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Log.d(debugTag,"bluetoothTestAndEnable -- Attempting to enable bluetooth");
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            context.startActivity(enableBtIntent);
+        }
+        return bluetoothAdapter.isEnabled();
+    }
+
+    public ArrayList<BluetoothDevice> getPairedDevices() {
+        return pairedDevices;
+    }
+
+    public ArrayList<BluetoothDevice> getConnectedDevices() {
+        return connectedDevices;
+    }
 }
